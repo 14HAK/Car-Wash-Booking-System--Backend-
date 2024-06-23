ca49b913ea7b796ffd68ba644fc6930223afe64e
Dulon Mahadi <dulonmolla88@gmail.com>
2024-06-23 19:30:28 +0600

    Delete dist directory

--- dist/app/utils/zodValidation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import { ZodType } from 'zod';
-// import {TUSER} from '../modules/auth/user.interface';
-// const zodValidation = async (zodSchema: ZodType, rawData: TUSER) => {
-//   const isValidData = await zodSchema.parse(rawData);
-//   return isValidData
-// };
-// export default zodValidation;
